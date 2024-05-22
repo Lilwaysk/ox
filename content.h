0376168fcaac377b7a173ed59afe56b1dc8e990f
michi <1101462170@qq.com>
2024-05-23 07:53:18 +0800

    newsssss

--- content.h
+++ content.h
@@ -1,6 +1,10 @@
 #ifndef CONTENT_H
 #define CONTENT_H
 
+#include "baseinfo.h"
+#include "contact.h"
+#include "detail.h"
+
 #include <QFrame>
 #include <QStackedWidget>
 #include <QPushButton>
@@ -10,7 +14,12 @@ class Content : public QFrame
 {
 public:
     explicit Content(QFrame *parent = nullptr);
-
+    QStackedWidget *stack;
+    QPushButton *AmendBtn;
+    QPushButton *CloseBtn;
+    BaseInfo *baseInfo;
+    Contact *contact;
+    Detail *detail;
 signals:
 
 };
