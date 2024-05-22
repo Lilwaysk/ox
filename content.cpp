7aa037b8cdb634d9533e5b9153c4e798b7e0aaae
michi <1101462170@qq.com>
2024-05-23 07:58:12 +0800

    newssssss

--- content.cpp
+++ content.cpp
@@ -1,6 +1,25 @@
 #include "content.h"
+#include "baseinfo.h"
+#include "contact.h"
+#include "detail.h"
+
+#include <QFrame>
+#include <QStackedWidget>
+#include <QPushButton>
+#include <QLabel>
 
 Content::Content(QFrame *parent) : QFrame(parent)
 {
+    stack = new QStackedWidget(this);
+    stack->setFrameStyle(QFrame::Panel|QFrame::Raised);
 
+    /*Insert Three Page Windows*/
+
+    baseInfo = new BaseInfo();
+    contact = new Contact();
+    detail = new Detail();
+
+    stack->addWidget(baseInfo);
+    stack->addWidget(contact);
+    stack->addWidget(detail);
 }
