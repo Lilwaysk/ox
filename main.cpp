514c6eb1e54295a7573b88b3c0dd1e0c403e2012
michi <1101462170@qq.com>
2024-05-22 21:58:57 +0800

    yes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "mainwindow.h"
 #include "login.h"
+#include "sql.h"
 
 #include <QApplication>
+
+void test()
+{
+    // login l;
+    // l.show();
+    sql s;
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,8 +19,7 @@ int main(int argc, char *argv[])
     Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
     QApplication a(argc, argv);
     MainWindow w;
-    login l;
-    l.show();
+    test();
     w.setWindowIcon(QIcon(":/new/prefix1/pic/pic/1.png"));
     w.show();
     return a.exec();
--- sql.cpp
+++ sql.cpp
@@ -14,17 +14,29 @@ QSqlQuery query(db);
 
 sql::sql()
 {
-
+    sql_connect();
+    sql_create();
 }
 
 void sql::sql_connect()
 {
-
+    db.setHostName("localhost");
+    db.setUserName("root");
+    db.setPassword("1234");
+    db.setDatabaseName("Qdb");
+    if(!db.open())
+        qDebug() << "未成功链接到数据库";
+    else
+        qDebug() << "成功链接到数据库";
 }
 
 void sql::sql_create()
 {
-
+    bool suc = query.exec("create table user(id int primary key auto_increment,username varchar(255),password varchar(20))");
+    if (suc)
+        qDebug() << "创建成功" << endl;
+    else
+        qDebug() << "创建失败" << endl;
 }
 
 void sql::sql_init()
