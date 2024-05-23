16c91e3761b20b3b018aa804242c40ccb7dc4b99
michi <1101462170@qq.com>
2024-05-23 08:15:29 +0800

    yy

--- baseinfo.h
+++ baseinfo.h
@@ -2,6 +2,11 @@
 #define BASEINFO_H
 
 #include <QWidget>
+#include <QComboBox>
+#include <QTextEdit>
+#include <QLabel>
+#include <QGridLayout>
+#include <QPushButton>
 
 class BaseInfo : public QWidget
 {
@@ -10,6 +15,27 @@ public:
 
 signals:
 
+private:
+    // Left
+    QLabel *UserNameLabel;
+    QLabel *NameLabel;
+    QLabel *SexLabel;
+    QLabel *DepartmentLabel;
+    QLabel *AgeLabel;
+    QLabel *OtherLabel;
+    QLineEdit *UserNameLineEdit;
+    QLineEdit *NameLineEdit;
+    QComboBox *SexComboBox;
+    QTextEdit *DepartmentTextEdit;
+    QLineEdit *AgeLineEdit;
+    QGridLayout *LeftLayout;
+    // Right
+    QLabel *HeadLabel;
+    QLabel *HeadIconLabel;
+    QPushButton *UpdateHeadBtn;
+    QHBoxLayout *TopRightLayout;
+    QLabel *IntroductionTextEdit;
+    QVBoxLayout *RightLayout;
 };
 
 #endif // BASEINFO_H
