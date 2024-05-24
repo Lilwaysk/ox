c8414534581c6f02ca1dcd0e63a17f25fe472459
michi <1101462170@qq.com>
2024-05-24 08:13:19 +0800

    new

--- detail.h
+++ detail.h
@@ -2,6 +2,11 @@
 #define DETAIL_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QTextEdit>
+#include <QGridLayout>
 
 class Detail : public QWidget
 {
@@ -10,6 +15,16 @@ public:
 
 signals:
 
+private:
+    QLabel *NationalLabel;
+    QComboBox *NationalComboBox;
+    QLabel *ProvinceLabel;
+    QComboBox *ProvinceComboBox;
+    QLabel *CityLabel;
+    QLineEdit *CityComboBox;
+    QLabel *IntroductLabel;
+    QTextEdit *IntroductTextEdit;
+    QGridLayout *mainLayout;
 };
 
 #endif // DETAIL_H
